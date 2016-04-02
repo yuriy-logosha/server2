ac16286eebc6fbd6b978ac4c99e1622546ca80f3
Logosha, Iurii <iurii.logosha@accenture.com>
2016-04-02 23:33:25 +0300

    Add unexisting files and formatter

---
+++ src/main/java/com/sslv/services/StreetAddress.java
@@ -0,0 +1,34 @@
+package com.sslv.services;
+
+public class StreetAddress {
+	
+	private String placeId;
+	private String address;
+	
+	private double[] location;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPlaceId() {
+		return placeId;
+	}
+
+	public void setPlaceId(String placeId) {
+		this.placeId = placeId;
+	}
+
+	public double[] getLocation() {
+		return location;
+	}
+
+	public void setLocation(double[] location) {
+		this.location = location;
+	}
+
+}
