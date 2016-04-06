11904ac85a5076532b3de9f83e2776c38bde1bb5
Logosha, Iurii <yuriy.logosha@gmail.com>
2016-04-06 23:49:32 +0300

    Fix jar name

--- src/main/resources/cron.sh
+++ src/main/resources/cron.sh
@@ -1,7 +1,7 @@
 User cron:
 
 */5 * * * * cd /home/ubuntu/sslv; mvn test -Dscope=today -Dthreads=10
-/usr/bin/java -Dscope=today -Dthreads=10 -Xms256m -Xmx1g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -jar /home/ubuntu/sslv/target/sslv-0.0.1-SNAPSHOT-jar-with-dependencies.jar start -d -p /var/run/sslvlauncher/sslvlauncher.pid
+/usr/bin/java -Dscope=today -Dthreads=10 -Xms256m -Xmx1g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError -XX:+DisableExplicitGC -jar /home/ubuntu/sslv/target/sslv-0.0.1-SNAPSHOT-with-dependencies.jar start -d -p /var/run/sslvlauncher/sslvlauncher.pid
 
 Root crons:
 */10 * * * * sudo service kibana start >> /home/ubuntu/kibana-cron-log
